97187400c9c8f340193749e0522cf93323bc0d6a
Emma Gerigscott <elgerig@gmail.com>
2023-06-09 09:52:38 -0700

    add get route for animals + search queries

---
+++ AnimalShelterApi/Controllers/AnimalsController.cs
@@ -0,0 +1,49 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using AnimalShelterApi.Models;
+
+namespace AnimalShelterApi.Controllers
+{
+  [Route("api/v{version:apiVersion}/[controller]")]
+  [ApiController]
+  [ApiVersion("1.0")]
+
+  public class AnimalsController: ControllerBase
+  {
+    private readonly AnimalShelterApiContext _db;
+    public AnimalsController(AnimalShelterApiContext db)
+    {
+      _db = db;
+    }
+
+    [HttpGet]
+    public async Task<List<Animal>> Get(string name, string type, string breed, DateTime? date = null, bool? available = null)
+    {
+      IQueryable<Animal> query = _db.Animals
+                                    .AsQueryable();
+
+      if (name != null)
+      {
+        query = query.Where(entry => entry.Name == name);
+      }
+      if (type != null)
+      {
+        query = query.Where(entry => entry.Type == type);
+      }
+      if (breed != null)
+      {
+        query = query.Where(entry => entry.Breed == breed);
+      }
+      if (date.HasValue)
+      {
+        query = query.Where(entry => entry.DateAdmitted == date);
+      }
+      if (available.HasValue)
+      {
+        query = query.Where(entry => entry.Available == available);
+      }
+
+      return await query.ToListAsync();
+    }
+  }
+}
