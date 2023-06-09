023ee5c6ea6af707d27b339b7b03df643971c100
Emma Gerigscott <elgerig@gmail.com>
2023-06-09 12:59:33 -0700

    add dependencyinjection file in startup

--- AnimalShelterApi/Program.cs
+++ AnimalShelterApi/Program.cs
@@ -1,6 +1,6 @@
 using AnimalShelterApi.Models;
+using AnimalShelterApi.Startup;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Mvc.Versioning;
 using Microsoft.AspNetCore.Mvc.ApiExplorer;
 using Microsoft.OpenApi.Models;
 using Microsoft.Extensions.Options;
@@ -9,7 +9,7 @@ using Swashbuckle.AspNetCore.SwaggerGen;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
+builder.Services.RegisterServices();
 
 builder.Services.AddDbContext<AnimalShelterApiContext>(
                   dbContextOptions => dbContextOptions
@@ -19,31 +19,6 @@ builder.Services.AddDbContext<AnimalShelterApiContext>(
                     )
                   )
                 );
-                
-builder.Services.AddAuthentication();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddApiVersioning(opt =>
-                                    {
-                                      opt.DefaultApiVersion = new Microsoft.AspNetCore.Mvc.ApiVersion(1, 0);
-                                      opt.AssumeDefaultVersionWhenUnspecified = true;
-                                      opt.ReportApiVersions = true;
-                                      opt.ApiVersionReader = ApiVersionReader.Combine(new UrlSegmentApiVersionReader(),
-                                                                                      new HeaderApiVersionReader("x-api-version"),
-                                                                                      new MediaTypeApiVersionReader("x-api-version"));
-                                    });
-
-builder.Services.AddVersionedApiExplorer(setup =>
-{
-    setup.GroupNameFormat = "'v'VVV";
-    setup.SubstituteApiVersionInUrl = true;
-});
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.ConfigureOptions<ConfigureSwaggerOptions>();
 
 var app = builder.Build();
 
