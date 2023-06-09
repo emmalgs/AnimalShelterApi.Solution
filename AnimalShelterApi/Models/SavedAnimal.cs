e9c9ed6657d131898fea17fae01281754f00492a
Emma Gerigscott <elgerig@gmail.com>
2023-06-09 08:57:32 -0700

    add join table of saved animals

---
+++ AnimalShelterApi/Models/SavedAnimal.cs
@@ -0,0 +1,9 @@
+namespace AnimalShelterApi.Models
+{
+  public class SavedAnimal
+  {
+    public int SavedAnimalId { get; set; }
+    public string UserId { get; set; }
+    public int AnimalId { get; set; }
+  }
+}
