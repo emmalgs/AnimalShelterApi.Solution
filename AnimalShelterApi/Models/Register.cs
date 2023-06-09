5c1e4d5ccc651a1ab0e1cb506c66771e933d4078
Emma Gerigscott <elgerig@gmail.com>
2023-06-09 11:05:31 -0700

    add register model to user identity for users

---
+++ AnimalShelterApi/Models/Register.cs
@@ -0,0 +1,8 @@
+namespace AnimalShelterApi.Models;
+
+public class Register
+{
+  public string Username { get; set; }
+  public string Email { get; set; }
+  public string Password { get; set; }
+}
